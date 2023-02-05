da51ac5ef0810c8dae77d886cbcd442569d53207
Alzound <aaxlcd@gmail.com>
2023-02-05 12:41:17 -0600

    Menu y camaras.

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,40 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class Menu : MonoBehaviour
 {
     public GameObject menu; 
     public GameObject credits;
-    public Player_Controller player; 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject buttons, returnButton; 
+    //public Player_Controller player; 
 
     public void Play()
     {
-        menu.SetActive(false); 
-        player.game = true; 
+        SceneManager.LoadScene(1);
     }
 
     public void Credits()
     {
         credits.SetActive(true); 
+        returnButton.SetActive(true);
     }
 
     public void Return()
     {
         credits.SetActive(false);
-        menu.SetActive(true);
+        buttons.SetActive(true);
+        returnButton.SetActive(false);   
     }
 
     public void Quit()
